3ace828d29457d66c7512fad1e2407f819ab2175
Leonardo Acevedo <leo.acevedo@gmail.com>
2020-09-22 01:03:54 -0300

    Updates

---
+++ precache-manifest.eb8ba1912a43daff58ba7826f601cb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fbce4712efc25fcb2dc4d940e9e691",
+    "url": "/tc-scan-qr/index.html"
+  },
+  {
+    "revision": "c70ce0cdd03da8edef54",
+    "url": "/tc-scan-qr/static/css/main.c0498f38.chunk.css"
+  },
+  {
+    "revision": "92039c41ea65aa224ca6",
+    "url": "/tc-scan-qr/static/js/2.899bb552.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tc-scan-qr/static/js/2.899bb552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70ce0cdd03da8edef54",
+    "url": "/tc-scan-qr/static/js/main.4c817142.chunk.js"
+  },
+  {
+    "revision": "215dc20ba65204efe110",
+    "url": "/tc-scan-qr/static/js/runtime-main.e94a0855.js"
+  }
+]);
